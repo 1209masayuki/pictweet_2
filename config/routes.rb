f73ac1a60331e0d985da88aba235d65c2b8b0320
1209masayuki <xkiddo22@yahoo.co.jp>
2020-07-26 07:26:42 +0900

    errorの原因

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets　do
+  resources :tweets do
     resources :comments, only: :create
   end
   resources :users, only: :show
